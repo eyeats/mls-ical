75f1abf5345d60f44837dba76b4c43735cb20451
tylermachado <3586466+tylermachado@users.noreply.github.com>
2024-01-08 19:31:17 -0500

    add default express page

--- app.js
+++ app.js
@@ -5,6 +5,11 @@ const clubsData = require('./data/clubs.json');
 const app = express();
 const port = 3003;
 
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+  res.sendStatus(200);
+});
+
 app.get("/cron", (req, res) => {
   clubsData.forEach(club => {
     generateCals(club.abbreviation);
