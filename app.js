3dca063035e9187c31eacd5e6d9eed4ea21e5139
tylermachado <3586466+tylermachado@users.noreply.github.com>
2024-01-08 19:28:35 -0500

    new approach to filtering

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
-const express = require('express')
-const generateCals = require('./generateCals')
+const express = require('express');
+const generateCals = require('./generateCals');
+const clubsData = require('./data/clubs.json');
 
-const app = express()
-const port = 3003
+const app = express();
+const port = 3003;
 
 app.get("/cron", (req, res) => {
-  generateCals();
-
-  res.sendStatus(200) // sends an "OK" response
-})
+  clubsData.forEach(club => {
+    generateCals(club.abbreviation);
+  });
+  res.sendStatus(200);
+});
--- generateCals.js
+++ generateCals.js
@@ -1,19 +1,17 @@
-const path = require('path');
 const fs = require('fs');
 const ics = require('ics');
 const fetch = require('node-fetch');
-const clubsData = require('./data/clubs.json')
-
-const directoryPath = path.join(__dirname, 'data');
+const clubsData = require('./data/clubs.json');
 
 fetch('https://sportapi.mlssoccer.com/api/matches?culture=en-us&dateFrom=2024-01-01&dateTo=2024-12-31&excludeSecondaryTeams=true')
   .then((response) => response.text())
   .then((body) => {
     const matches = JSON.parse(body);
-    const formattedMatches = [];
-    matches.forEach(match => {
-      clubsData.forEach(club => {
-        if (match.home.abbreviation === club.abbreviation || match.away.abbreviation === club.abbreviation) {
+    clubsData.forEach(club => {
+      const formattedMatches = [];
+      matches
+        .filter(match => (match.home.abbreviation === club.abbreviation || match.away.abbreviation === club.abbreviation))
+        .forEach(match => {
           const matchData = new Object;
           const matchDate = new Date(match.matchDate);
 
@@ -35,10 +33,12 @@ fetch('https://sportapi.mlssoccer.com/api/matches?culture=en-us&dateFrom=2024-01
 
             fs.writeFile('public/' + club.abbreviation + '.ics', value, (err) => {
               if (err) throw err;
-              console.log('.ics calendar file saved');
+              console.log(club.abbreviation + '.ics calendar file saved');
             });
           }
-        }
-      });
-    });
-  });
+        }); // matches.forEach
+    }); // clubsData.forEach
+  }); // .then
+
+
+
