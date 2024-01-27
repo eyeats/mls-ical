11c1195de5f1ab00aab0b355e5664e457d093ca4
tylermachado <3586466+tylermachado@users.noreply.github.com>
2024-01-27 18:14:27 -0500

    check

---
+++ server.js
@@ -0,0 +1,17 @@
+// server.js
+// where your node app starts
+
+// init project
+const express = require("express");
+const app = express();
+
+app.use(express.static("public"));
+
+app.get("/", function(request, response) {
+  response.sendFile(__dirname + "/public/NE.ics");
+});
+
+// listen for requests :)
+const listener = app.listen(process.env.PORT, function() {
+  console.log("Your app is listening on port " + listener.address().port);
+});
