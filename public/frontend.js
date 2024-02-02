b1d7aa3dbef99f644fddf925dbdd265cc07527aa
tylermachado <3586466+tylermachado@users.noreply.github.com>
2024-02-02 16:38:37 -0500

    rearrange

--- public/frontend.js
+++ public/frontend.js
@@ -1,120 +1,178 @@
 const clubsData = [
-    {
-        "club": "Atlanta",
-        "abbreviation": "ATL"
-    },
-    {
-        "club": "Austin",
-        "abbreviation": "ATX"
-    },
-    {
-        "club": "Charlotte",
-        "abbreviation": "CLT"
-    },
-    {
-        "club": "Chicago",
-        "abbreviation": "CHI"
-    },
-    {
-        "club": "Cincinnati",
-        "abbreviation": "CIN"
-    },
-    {
-        "club": "Colorado",
-        "abbreviation": "COL"
-    },
-    {
-        "club": "Columbus",
-        "abbreviation": "CLB"
-    },
-    {
-        "club": "Dallas",
-        "abbreviation": "DAL"
-    },
-    {
-        "club": "DC",
-        "abbreviation": "DC"
-    },
-    {
-        "club": "Houston",
-        "abbreviation": "HOU"
-    },
-    {
-        "club": "Kansas City",
-        "abbreviation": "SKC"
-    },
-    {
-        "club": "LA Galaxy",
-        "abbreviation": "LA"
-    },
-    {
-        "club": "LAFC",
-        "abbreviation": "LAFC"
-    },
-    {
-        "club": "Miami",
-        "abbreviation": "MIA"
-    },
-    {
-        "club": "Minnesota",
-        "abbreviation": "MIN"
-    },
-    {
-        "club": "Montreal",
-        "abbreviation": "MTL"
-    },
-    {
-        "club": "Nashville",
-        "abbreviation": "NSH"
-    },
-    {
-        "club": "New England",
-        "abbreviation": "NE"
-    },
-    {
-        "club": "NYCFC",
-        "abbreviation": "NYC"
-    },
-    {
-        "club": "NY Red Bulls",
-        "abbreviation": "RBNY"
-    },
-    {
-        "club": "Orlando",
-        "abbreviation": "ORL"
-    },
-    {
-        "club": "Philadelphia",
-        "abbreviation": "PHI"
-    },
-    {
-        "club": "Portland",
-        "abbreviation": "POR"
-    },
-    {
-        "club": "Salt Lake",
-        "abbreviation": "RSL"
-    },
-    {
-        "club": "San Jose",
-        "abbreviation": "SJ"
-    },
-    {
-        "club": "Seattle",
-        "abbreviation": "SEA"
-    },
-    {
-        "club": "St. Louis",
-        "abbreviation": "STL"
-    },
-    {
-        "club": "Toronto",
-        "abbreviation": "TOR"
-    },
-    {
-        "club": "Vancouver",
-        "abbreviation": "VAN"
-    }
+	{
+		"club": "Atlanta",
+		"abbreviation": "ATL",
+		"fullname": "Atlanta United FC",
+		"shortname": "Atlanta United"
+	},
+	{
+		"club": "Austin",
+		"abbreviation": "ATX",
+		"fullname": "Austin FC",
+		"shortname": "Austin FC"
+	},
+	{
+		"club": "Charlotte",
+		"abbreviation": "CLT",
+		"fullname": "Charlotte FC",
+		"shortname": "Charlotte FC"
+	},
+	{
+		"club": "Chicago",
+		"abbreviation": "CHI",
+		"fullname": "Chicago Fire FC",
+		"shortname": "Chicago Fire"
+	},
+	{
+		"club": "Cincinnati",
+		"abbreviation": "CIN",
+		"fullname": "FC Cincinnati",
+		"shortname": "FC Cincinnati"
+	},
+	{
+		"club": "Colorado",
+		"abbreviation": "COL",
+		"fullname": "Colorado Rapids",
+		"shortname": "Rapids"
+	},
+	{
+		"club": "Columbus",
+		"abbreviation": "CLB",
+		"fullname": "Columbus Crew",
+		"shortname": "Columbus Crew"
+	},
+	{
+		"club": "Dallas",
+		"abbreviation": "DAL",
+		"fullname": "FC Dallas",
+		"shortname": "FC Dallas"
+	},
+	{
+		"club": "DC",
+		"abbreviation": "DC",
+		"fullname": "D.C. United",
+		"shortname": "D.C. United"
+	},
+	{
+		"club": "Houston",
+		"abbreviation": "HOU",
+		"fullname": "Houston Dynamo FC",
+		"shortname": "Dynamo"
+	},
+	{
+		"club": "Kansas City",
+		"abbreviation": "SKC",
+		"fullname": "Sporting Kansas City",
+		"shortname": "Sporting KC"
+	},
+	{
+		"club": "LA Galaxy",
+		"abbreviation": "LA",
+		"fullname": "LA Galaxy",
+		"shortname": "LA Galaxy"
+	},
+	{
+		"club": "LAFC",
+		"abbreviation": "LAFC",
+		"fullname": "Los Angeles Football Club",
+		"shortname": "LAFC"
+	},
+	{
+		"club": "Miami",
+		"abbreviation": "MIA",
+		"fullname": "Inter Miami CF",
+		"shortname": "Inter Miami"
+	},
+	{
+		"club": "Minnesota",
+		"abbreviation": "MIN",
+		"fullname": "Minnesota United FC",
+		"shortname": "Minnesota United"
+	},
+	{
+		"club": "Montreal",
+		"abbreviation": "MTL",
+		"fullname": "CF Montréal",
+		"shortname": "CF Montréal"
+	},
+	{
+		"club": "Nashville",
+		"abbreviation": "NSH",
+		"fullname": "Nashville SC",
+		"shortname": "Nashville SC"
+	},
+	{
+		"club": "New England",
+		"abbreviation": "NE",
+		"fullname": "New England Revolution",
+		"shortname": "Revolution"
+	},
+	{
+		"club": "NYCFC",
+		"abbreviation": "NYC",
+		"fullname": "New York City FC",
+		"shortname": "NYCFC"
+	},
+	{
+		"club": "NY Red Bulls",
+		"abbreviation": "RBNY",
+		"fullname": "New York Red Bulls",
+		"shortname": "Red Bulls"
+	},
+	{
+		"club": "Orlando",
+		"abbreviation": "ORL",
+		"fullname": "Orlando City SC",
+		"shortname": "Orlando City"
+	},
+	{
+		"club": "Philadelphia",
+		"abbreviation": "PHI",
+		"fullname": "Philadelphia Union",
+		"shortname": "Phila Union"
+	},
+	{
+		"club": "Portland",
+		"abbreviation": "POR",
+		"fullname": "Portland Timbers",
+		"shortname": "Timbers"
+	},
+	{
+		"club": "Salt Lake",
+		"abbreviation": "RSL",
+		"fullname": "Real Salt Lake",
+		"shortname": "Real Salt Lake"
+	},
+	{
+		"club": "San Jose",
+		"abbreviation": "SJ",
+		"fullname": "San Jose Earthquakes",
+		"shortname": "Earthquakes"
+	},
+	{
+		"club": "Seattle",
+		"abbreviation": "SEA",
+		"fullname": "Seattle Sounders FC",
+		"shortname": "Sounders"
+	},
+	{
+		"club": "St. Louis",
+		"abbreviation": "STL",
+		"fullname": "St. Louis CITY SC",
+		"shortname": "St. Louis City"
+	},
+	{
+		"club": "Toronto",
+		"abbreviation": "TOR",
+		"fullname": "Toronto FC",
+		"shortname": "Toronto FC"
+	},
+	{
+		"club": "Vancouver",
+		"abbreviation": "VAN",
+		"fullname": "Vancouver Whitecaps FC",
+		"shortname": "Whitecaps"
+	}
 ];
 
 // get element with id of 'links'
@@ -122,10 +180,24 @@ const linksContainer = document.getElementById('links');
 
 // for each club, add a list item and a link to its .ics file
 clubsData.forEach(club => {
-  const listItem = document.createElement('li');
-  const link = document.createElement('a');
-  link.href = `webcal://${window.location.host}/${club.abbreviation}.ics`;
-  link.innerText = club.club;
-  listItem.appendChild(link);
-  linksContainer.appendChild(listItem);
+  const clubContainer = document.createElement('div');
+  const clubHeading = document.createElement('h2');
+  clubHeading.innerText = club.fullname;
+  clubContainer.appendChild(clubHeading);
+
+  const url = `webcal://${window.location.host}/${club.abbreviation}.ics`;
+  let links = new Map();
+  links.set('apple', url);
+  links.set('google', `https://calendar.google.com/calendar/u/0/r?cid=${url}`);
+  links.set('outlook', `https://outlook.office.com/calendar/addfromweb?url=${url}&name=${club.fullname}%20Schedule`);
+  links.set('other', url);
+
+  for (let [key, value] of links) {
+	  const linkButton = document.createElement('a');
+	  linkButton.href = value;
+	  linkButton.innerText = key;
+	  clubContainer.appendChild(linkButton);
+  }
+
+  linksContainer.appendChild(clubContainer);
 });
